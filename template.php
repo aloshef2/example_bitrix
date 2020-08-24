02d8ab69d669dc52eeec2d83be87c8256ff5bd5f
aloshef2 <49838049+aloshef2@users.noreply.github.com>
2020-08-24 12:20:25 +0700

    Add files via upload

--- template.php
+++ template.php
@@ -1,68 +1,105 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
-$this->setFrameMode(true);
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 ?>
-<div id="barba-wrapper">
-<div class="article-list">
-	<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-		<?=$arResult["NAV_STRING"]?><br />
-	<?endif;?>
-		<?foreach($arResult["ITEMS"] as $arItem):?>
-			<?
-			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-			?>
-			<a id="<?=$this->GetEditAreaId($arItem['ID']);?>" href="<?=$arItem["DETAIL_PAGE_URL"]?>">
-				<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-					<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-						<div class="article-item__background">
-							<img
-								border="0"
-								src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-								alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-								title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-							/>
-						</div>
-					<?else:?>
-						<div class="article-item__background">
-							<img
-								border="0"
-								src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-								alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-								title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-								/>
-						</div>
-					<?endif;?>
-				<?endif?>
-				<div class="article-item__wrapper">
-					<div class="article-item__title">
-						<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-							<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-								<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
-							<?else:?>
-								<?echo $arItem["NAME"]?>
-						<?endif;?>
-					</div>
-					<div class="article-item__content">
-						<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-								<?echo $arItem["PREVIEW_TEXT"];?>
-						<?endif;?>
-					</div>
-				</div>
-			</a>
-		<?endforeach;?>
+<?if ($arResult["isFormErrors"] == "Y"):?><?=$arResult["FORM_ERRORS_TEXT"];?><?endif;?>
+
+<?=$arResult["FORM_NOTE"]?>
+
+<?if ($arResult["isFormNote"] != "Y")
+{
+?>
+
+<div class="contact-form">
+    <div class="contact-form__head">
+        <div class="contact-form__head-title">Связаться</div>
+        <div class="contact-form__head-text">Наши сотрудники помогут выполнить подбор услуги и&nbsp;расчет цены с&nbsp;учетом
+            ваших требований
+        </div>
+    </div>
+	<?=str_replace('<form','<form class="contact-form__form"', $arResult["FORM_HEADER"])?>
+        <div class="contact-form__form-inputs">
+		<?
+			foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion)
+			{
+				if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'hidden')
+				{
+					echo $arQuestion["HTML_CODE"];
+				}
+				else
+				{
+					$class = 'input contact-form__input';
+					if($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == text)
+					{
+						?>
+							<div class="input contact-form__input">
+								<label class="input__label" for="medicine_name">
+									<div class="input__label-text">
+										<?=$arQuestion["CAPTION"]?><?if ($arQuestion["REQUIRED"] == "Y"):?><?=$arResult["REQUIRED_SIGN"];?><?endif;?></div>
+									<?= $arQuestion["HTML_CODE"] ?>
+								</label>
+							</div>
+						<?
+					}
+					if($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == textarea)
+					{
+						break;
+					}
+				}
+			} //endwhile
+		?>
 		</div>
-	<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-		<br /><?=$arResult["NAV_STRING"]?>
-	<?endif;?>
+		<div class="contact-form__form-message">
+			<div class="input">
+				<label class="input__label" for="medicine_message">
+					<div class="input__label-text">Сообщение</div>
+						<?
+							foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion)
+							{
+								if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'hidden')
+								{
+									echo $arQuestion["HTML_CODE"];
+								}
+								else
+								{
+									if($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == textarea)
+									{
+										
+										?>
+										<?=str_replace('<textarea','<textarea class="input__input"', $arQuestion["HTML_CODE"])  ?>
+										<?
+									}
+								}
+							} //endwhile
+						?>
+					<div class="input__notification"></div>
+				</label>
+			</div>
+		</div>
+		<?
+		if($arResult["isUseCaptcha"] == "Y")
+			{
+		?>
+			<?=GetMessage("FORM_CAPTCHA_TABLE_TITLE")?>
+			<input type="hidden" name="captcha_sid" value="<?=htmlspecialcharsbx($arResult["CAPTCHACode"]);?>" /><img src="/bitrix/tools/captcha.php?captcha_sid=<?=htmlspecialcharsbx($arResult["CAPTCHACode"]);?>" width="180" height="40" />
+
+			<?=GetMessage("FORM_CAPTCHA_FIELD_TITLE")?><?=$arResult["REQUIRED_SIGN"];?>
+			<input type="text" name="captcha_word" size="30" maxlength="50" value="" class="inputtext" />
+		<?
+			} // isUseCaptcha
+		?>
+        
+	
+	
 </div>
+<div class="contact-form__bottom">
+            <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
+                ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных
+                данных&raquo;.
+            </div>
+			<button class="form-button contact-form__bottom-button" data-success="Отправлено" data-error="Ошибка отправки" type="submit" name="web_form_submit" value="Оставить заявку" >Оставить заявку</button>
+
+        </div>
+<?=$arResult["FORM_FOOTER"]?>
+<?
+} //endif (isFormNote)
+?>
