08db459041339768ac3b26a8bccacc621eda1312
aloshef2 <49838049+aloshef2@users.noreply.github.com>
2020-08-28 15:07:27 +0700

    Add files via upload

---
+++ example/description.php
@@ -0,0 +1,12 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$arComponentDescription = array(
+	"NAME" => "Задание3",
+	"COMPLEX" => "Y",
+	"PATH" => array(
+        "ID" => "exp3",
+        "NAME" => "Задание3",
+	),
+);
+
+?>
