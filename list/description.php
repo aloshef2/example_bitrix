ea378d29766df0d4629dc0a5d0ad6042467ac024
aloshef2 <49838049+aloshef2@users.noreply.github.com>
2020-09-17 08:53:40 +0700

    Add files via upload

---
+++ list/description.php
@@ -0,0 +1,12 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$arComponentDescription  = array(
+    "NAME" => "Задание_шестое",
+    "DESCRIPTION" => "list",
+    "PATH" => array(
+        "ID" => "list",
+        "NAME" => "Список"
+    )
+);
+
+?>
